95e8b430f926fa1c033f6eb38fb0a282a93c8ab7
E <eliasandrov@gmail.com>
2021-03-23 21:53:54 +0200

    test 1

---
+++ untitled1/src/main/java/example/test1.java
@@ -0,0 +1,19 @@
+package example;
+
+import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class test1 {
+    private static final Logger logger = LoggerFactory.getLogger(test1.class);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("STARTING SERVER....");
+
+        test1 demo = new test1();
+
+    }
+
+
+}
